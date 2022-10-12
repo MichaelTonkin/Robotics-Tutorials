16f0469c2e5d4458067e571f73be4d25ea459a80
Michael Tonkin <michaeltonkin99@gmail.com>
2022-10-12 19:01:06 +0100

    Pre refactor

--- Labsheet_3/Labsheet_3.ino
+++ Labsheet_3/Labsheet_3.ino
@@ -2,21 +2,25 @@
 # define LSEN_CENTRE_IN_PIN A2
 # define LSEN_RIGHT_IN_PIN A3
 # define EMIT 11
+# define MAX_LSEN_PIN 3
 # define SECONDS 1000
 
-void setup() 
-{
-  
-  enableLineSensors();
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("It has begin...");
+int lsen_pin[MAX_LSEN_PIN] = {LSEN_LEFT_IN_PIN, LSEN_CENTRE_IN_PIN, LSEN_RIGHT_IN_PIN};
 
+void setup() 
+{  
+  enableLineSensors();
+  beginLogging();
 }
 
 void loop() 
 {
   lineSensorLoop();
+}
+
+void lineSensorLoop()
+{
+  waitUntilVoltsLow();
 }
 
 void enableLineSensors()
@@ -27,82 +31,108 @@ void enableLineSensors()
   pinMode(LSEN_RIGHT_IN_PIN, INPUT);
 }
 
-void lineSensorLoop()
-{
-  chargeCapacitor();
-  waitUntilVoltsLow();
-}
-
-void chargeCapacitor()
+void chargeCapacitor(int pin)
 {
   // Charge capacitor by setting input pin
   // temporarily to output and HIGH
-  pinMode( LSEN_LEFT_IN_PIN, OUTPUT );
-  digitalWrite( LSEN_LEFT_IN_PIN, HIGH );
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, HIGH );
 
   // Tiny delay for capacitor to charge.
   delayMicroseconds(10);
 
   //  Turn input pin back to an input
-  pinMode( LSEN_LEFT_IN_PIN, INPUT );
-  
+  pinMode( pin, INPUT );
 }
 
 void waitUntilVoltsLow()
 {
-  unsigned long elapsed_time;
-    // Places to store microsecond count
-  unsigned long start_time; // t_1
-  unsigned long end_time;   // t_2
-
-  countTime(start_time, end_time);
+  unsigned long sensor_time;
+  sensor_time = countTime();
+  printElapsedTime(sensor_time);
+  delay(100);
 }
 
-void countTime(unsigned long start_time, unsigned long end_time)
+unsigned long countTime()
 {
-
-  unsigned long sensor_time;
-
-  start_time = micros();
-
+  //unsigned long sensor_time;
+  unsigned long start_time = micros();
+  unsigned long end_time;
+  unsigned long elapsed_time;
+  unsigned long current_time;
+  unsigned long sensor_read[MAX_LSEN_PIN];
+  unsigned long timeout = 5 * SECONDS;
+  
   bool done = false;
 
-  unsigned long timeout = 5000;
-
-  while( done != true ) {
-
-    unsigned long current_time = micros();
-
-    unsigned long elapsed_time = current_time - start_time;
-
-    if( elapsed_time >= timeout ) {
-
-        done = true;
-        sensor_time = timeout;
-    }
-
-    if( digitalRead( LSEN_LEFT_IN_PIN ) == LOW ) {
-
-        sensor_time = elapsed_time;
-        done = true;
-    }
-
+  int remaining = MAX_LSEN_PIN;
+//new code needs to go here
+      //we need to charge each capacitor
+    //and reconfigure the sensor pins to be read again
+  
+  for(int i = 0; i < MAX_LSEN_PIN; i++)
+  {
+    chargeCapacitor(lsen_pin[i]);
   }
 
-  printElapsedTime(sensor_time);
+  while( remaining > 0)
+  {
+    for (int w = 0; w < MAX_LSEN_PIN; w++) 
+    {
+      if(pinIsLow(lsen_pin[w])) {
+          if(sensor_read[w] == 0 || sensor_read[w] == NULL)
+          {
+            current_time = micros();
+            elapsed_time = calculateElapsedTime(start_time, current_time);
+            sensor_read[w] = elapsed_time;
+            remaining = remaining - 1;
+          }
+      }
+    }
+    if (timedOut(elapsed_time, timeout))
+    {
+      remaining = 0;
+      //Serial.println("--------------");
+      //Serial.println("TIMEOUT");
+      //Serial.println("--------------");
+    }
+  }
+  
+  return sensor_read;
+}
 
-  delay(100);
+bool pinIsLow(int pin)
+{
+  if( digitalRead( pin ) == LOW ) 
+  {
+    return true;
+  }
+  return false;
+}
+
+bool timedOut(unsigned long elapsed_time, unsigned long timeout)
+{
+    if( elapsed_time >= timeout ) 
+    {
+      return true;
+    }
+    return false;
 }
 
 unsigned long calculateElapsedTime(unsigned long start_time, unsigned long end_time)
 {
-  return end_time - start_time;;
+  return end_time - start_time;
 }
 
-void printElapsedTime(unsigned long elapsed_time)
+void printElapsedTime(unsigned long elapsed_time[])
 {
-  Serial.print("Left line sensor: " );
-  Serial.print( elapsed_time );
+  for (int i = 0; i < MAX_LSEN_PIN; i++)
+  {
+    Serial.print("Pin ");
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.println( elapsed_time[i] );
+  }
   Serial.print("\n");
 }
 
